46aa73ba0a97c91cf18f4850b16e498673636276
Daniel Tikamori <tikamori.itiro@gmail.com>
2025-06-29 20:38:52 +0900

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+package me.amlu
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.net.http.HttpResponse.BodyHandlers
+
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val client: HttpClient? = HttpClient.newHttpClient()
+    val request = HttpRequest.newBuilder()
+        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=612"))
+        .build()
+
+    val response: HttpResponse<String?>? = client!!
+        .send(request, BodyHandlers.ofString())
+
+    val json = response?.body()
+    println(json)
+
+}
